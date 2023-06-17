cea77e00913e0dd965237c58bf010d20a0b57abb
Muhammad Fadila <muh.fadila18@gmail.com>
2023-06-17 21:03:50 +0700

    COMMIT

---
+++ exercise_2.js
@@ -0,0 +1,7 @@
+video.addEventListener("click", function video() {
+    alert("ini video Yoasobi");
+});
+audio.addEventListener("click", function audio() { alert("ini Audio Yoasobi"); });
+gambar.addEventListener("click", function gambar() {
+    alert("ini gambar Yoasobi");
+});
